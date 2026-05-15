739c39bebaf43d62af9f70318960bebd42609d64
Localhorst04 <aaron.laib@protonmail.com>
2026-05-15 13:38:37 +0000

    test
    Change-Id: I1e3c69a5bc2a220063627153beef19544510eb2a

--- tokay/axion_flags.mk
+++ tokay/axion_flags.mk
@@ -2,11 +2,12 @@
 TARGET_ENABLE_BLUR := true
 
 # safely disable libperfmgr without breaking boot (set to true)
-TARGET_DISABLES_LIBPERF ?= false
+TARGET_DISABLES_LIBPERF ?= true
 
 # CPU governor support
 PERF_GOV_SUPPORTED := true
-PERF_DEFAULT_GOV := schedutil
+PERF_DEFAULT_GOV := sched_pixel
+PERF_ANIM_OVERRIDE := false
 
 TARGET_INCLUDE_AXFX := true
 
